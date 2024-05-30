04db0d701ff497e0ea46cf9cf1ac9afb1fa79a1c
Rehan Kaluarachchi <rehankalu@gmail.com>
2024-05-29 22:02:45 -0400

    Testing new formatting

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    let mvEventContainer2 = $('#mvEventContainer2');
+    let mvEventContainer2 = $("#mvEventContainer2");
     console.log(mvEventContainer2.find('table')[0])
   });
